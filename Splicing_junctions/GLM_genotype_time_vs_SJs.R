c9a2355d65b7f90b2689779d26bd78883f5459d9
vierocka <viera.kovacova@uni-koeln.de>
2025-10-16 16:50:18 +0200

    from bonferroni to fdr

--- Splicing_junctions/GLM_genotype_time_vs_SJs.R
+++ Splicing_junctions/GLM_genotype_time_vs_SJs.R
@@ -10,6 +10,7 @@ convTab <- read.csv("data_files/26169genes_with_AthalHomologs_allIDs_exprPattern
 SJoverview <- read.table("Splicing_junctions/SJ_ID_filteredCount_allDetected_allUniqMapInSTARalign.csv", header = FALSE, sep = "\t")
 colnames(SJoverview) <- c("sample","filteredSJs","rawSJs","librarySize")
 head(SJoverview)
+dim(SJoverview) # 36x 4
 
 # Create an offset by taking the log of the library size
 SJoverview$offset <- log(as.integer(SJoverview$librarySize))
@@ -47,7 +48,7 @@ SJoverview
 fit_poisson <- glm(SJoverview$filteredSJs ~ genotype*time + Batch + offset(SJoverview$offset), family = quasipoisson(link = "log"), data = SJoverview)
 dispersion <- sum(residuals(fit_poisson, type = "pearson")^2) / fit_poisson$df.residual
 dispersion
-# Dispersion: 1621.311 - much bigger than 1 (Poisson), huge even for quassi-Poisson
+# Dispersion: 1621.311 - much bigger than 1 (Poisson), huge even for quassi-Poisson; switched to negative binomial
 summary(fit_poisson)
 # Coefficients:
 # Estimate Std. Error t value Pr(>|t|)    
@@ -72,6 +73,7 @@ anova(fit_poisson, test = "Chisq")
 # Batch          1    362.6        29      41204   0.6363
 # genotype:time  6   3823.5        23      37381   0.8840
 
+#### Negative binomial
 fit_nb <- glm.nb(filteredSJs ~ genotype*timing + Batch + offset(offset), data = SJoverview)
 summary(fit_nb)
 # Coefficients:
@@ -218,99 +220,93 @@ hist(resultsNB[,1])
 
 colNam2
 # genotype
-adjPgen <- p.adjust(resultsNB[,15], method = "bonferroni")
+adjPgen <- p.adjust(resultsNB[,15], method = "fdr")
 # timing
-adjPtim <- p.adjust(resultsNB[,16], method = "bonferroni")
+adjPtim <- p.adjust(resultsNB[,16], method = "fdr")
 # interactions: genotype*timing
-adjPbatch <- p.adjust(resultsNB[,17], method = "bonferroni")
+adjPbatch <- p.adjust(resultsNB[,17], method = "fdr")
 # interactions: genotype*timing
-adjPgen_tim <- p.adjust(resultsNB[,18], method = "bonferroni")
+adjPgen_tim <- p.adjust(resultsNB[,18], method = "fdr")
 
 AdjPvalues <- as.data.frame(cbind(adjPgen, adjPtim, adjPgen_tim, adjPbatch))
 rownames(AdjPvalues) <- rownames(resultsNB)
 head(AdjPvalues)
 AdjPvalues[which(AdjPvalues$adjPbatch < 0.05),] 
-# 154 genes has significant batch; 140 TAIR homoolgs
+# 716 genes has significant batch; 615 TAIR homoolgs
 unique(sort(convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPbatch < 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
 
-# significant: time, genotype, their interaction; but not batch
+# significant: time, genotype, their interaction; but not batch - 19 genes
 AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim < 0.05 & AdjPvalues$adjPgen_tim < 0.05 & AdjPvalues$adjPbatch > 0.05),]
-# adjPgen      adjPtim adjPgen_tim adjPbatch
-# Vitvi04g00538 4.277933e-04 0.0089573906 0.007777987 0.8763858 - D7SUZ7 - AT2G25220 - Protein kinase superfamily protein
-# Vitvi19g00768 9.965880e-14 0.0002447435 0.015413210 0.0968189 - D7UAQ2 - AT1G55740 - SIP1 - seed imbibition 1
 convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim < 0.05 & AdjPvalues$adjPgen_tim < 0.05 & AdjPvalues$adjPbatch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), ]
 SJmat[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim < 0.05 & AdjPvalues$adjPgen_tim < 0.05 & AdjPvalues$adjPbatch > 0.05),]), rownames(SJmat)), ]
 resultsNB[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim < 0.05 & AdjPvalues$adjPgen_tim < 0.05 & AdjPvalues$adjPbatch > 0.05),]), rownames(resultsNB)), ]
+unique(sort(convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim < 0.05 & AdjPvalues$adjPgen_tim < 0.05 & AdjPvalues$adjPbatch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
+# AT1G32960,AT1G75280,AT2G05710,AT2G33530,AT2G35130,AT3G01510,AT3G20860,AT4G04790,AT4G10540,AT4G13550,AT4G17370,AT4G33050,AT5G05390,AT5G13980,AT5G44640,AT5G46570,AT5G51070,AT5G65940
+# A. thal homologs - string-db: UniProt - KW-0378	- Hydrolase - 9 of 2540	0.73	0.53; FDR=0.0052
 
 ###### general overview
-### only the interaction time*genotype significant: 25 genes
+### only the interaction time*genotype significant: 44 genes
 dim(AdjPvalues[which(AdjPvalues$adjPgen > 0.05 & AdjPvalues$adjPtim > 0.05 & AdjPvalues$adjPgen_tim < 0.05 & AdjPvalues$adjPbatch > 0.05),])
 convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen > 0.05 & AdjPvalues$adjPtim > 0.05 & AdjPvalues$adjPgen_tim < 0.05 & AdjPvalues$adjPbatch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]
 # Athal - homologs
-# AT1G68740,AT1G63460,AT5G46910,AT5G20060,AT3G03310,AT4G13550,AT5G13000,AT5G44640,AT1G61560,AT3G56160,AT1G66950,AT2G20340,AT4G39830,AT3G03620,AT1G56145,AT4G29230,AT1G54730,AT3G30300,AT3G28860,AT1G26810,AT1G19100,AT1G21310,AT4G10120,AT2G04235,AT5G23960
+# AT3G29770,AT1G10740,AT1G63460,AT4G35420,AT5G11030,AT5G17220,AT4G36470,AT3G23360,AT1G61560,AT3G50360,AT5G04730,AT1G61560,AT3G52870,AT3G57630,AT4G25150,AT1G30320,AT1G56145,AT1G11170,AT2G06040,AT5G47650,AT4G03500,AT5G07050,AT4G27190,AT4G27190,AT1G26810,AT1G14340,AT5G54160,AT3G18290,AT1G17840,AT3G17600,AT3G53760,AT5G43990,AT2G31580,AT5G48120,AT2G04235,AT1G49030,AT5G23960,AT1G11790,AT5G50950,AT3G18670,AT2G02380,AT1G51760,AT1G23030
 # no enrichment found
 
-### only time significant: 16 genes
+### only time significant: 23 genes
 dim(AdjPvalues[which(AdjPvalues$adjPgen > 0.05 & AdjPvalues$adjPtim < 0.05 & AdjPvalues$adjPgen_tim > 0.05 & AdjPvalues$adjPbatch > 0.05),])
 convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen > 0.05 & AdjPvalues$adjPtim < 0.05 & AdjPvalues$adjPgen_tim > 0.05 & AdjPvalues$adjPbatch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]
 # Atha - homologs
 # no enrichment found
 
-### only genotype significant: 763 genes (590 Athal unique TAIR homologs)
+### only genotype significant: 1948 genes (1219 Athal unique TAIR homologs)
 dim(AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim > 0.05 & AdjPvalues$adjPgen_tim > 0.05 & AdjPvalues$adjPbatch > 0.05),])
 unique(sort(convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim > 0.05 & AdjPvalues$adjPgen_tim > 0.05 & AdjPvalues$adjPbatch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
 
+# only differences in genotypes: 824 
 AdjPvalues[which(AdjPvalues$adjPgen < 0.001 & AdjPvalues$adjPtim > 0.05 & AdjPvalues$adjPgen_tim > 0.05 & AdjPvalues$adjPbatch > 0.05),]
-# only differences in genotypes: 449 (363 Athaliana unique homologs)
-unique(sort(convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.001 & AdjPvalues$adjPtim > 0.05 & AdjPvalues$adjPgen_tim > 0.05 & AdjPvalues$adjPbatch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
 
+# only differences in genotypes: 0
 AdjPvalues[which(AdjPvalues$adjPgen < 0.001 & AdjPvalues$adjPtim == 1 & AdjPvalues$adjPgen_tim == 1 & AdjPvalues$adjPbatch == 1),]
-# only differences in genotypes: 372 (296 Athaliana unique homologs)
-unique(sort(convTab[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.001 & AdjPvalues$adjPtim == 1 & AdjPvalues$adjPgen_tim == 1 & AdjPvalues$adjPbatch == 1),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
-# with A. thal homologs in string-db
-# PPI enrichment p-value:	8.55e-15
-# Biol. Pr. GO:0006629	Lipid metabolic process FDR=0.0001
-# GO:0050896	Response to stimulus 96 of 6206	0.16	0.27	FDR=0.0393
 
 # separate table for genotype-specific
 genotype_specific <- as.data.frame(resultsNB[match(rownames(AdjPvalues[which(AdjPvalues$adjPgen < 0.05 & AdjPvalues$adjPtim > 0.05 & AdjPvalues$adjPgen_tim > 0.05 & AdjPvalues$adjPbatch > 0.05),]), rownames(resultsNB)),])
 
-## RPV12 specific - 12 genes
+## RPV12 specific - 20 genes
 dim(genotype_specific[which(genotype_specific$RPV12 < 0.05 & genotype_specific$`RPV12+1` > 0.05 & genotype_specific$`RPV12+1+3` > 0.05 & genotype_specific$p_batch > 0.05),])
 unique(sort(convTab[match(rownames(genotype_specific[which(genotype_specific$RPV12 < 0.05 & genotype_specific$`RPV12+1`> 0.05 & genotype_specific$`RPV12+1+3` > 0.05 & genotype_specific$p_batch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
-# no enrichment detected (string-db)
-# AT1G56130,AT1G61010,AT2G34660,AT2G34930,AT4G27020,AT4G27040,AT5G16690,AT5G22820,AT5G63960,AT5G65210 (TF TGA1)
+# Athal homologs: AT1G02520,AT1G19640,AT1G21650,AT1G61010,AT1G76520,AT2G23470,AT2G34930,AT3G21250,AT3G49645,AT3G51570,AT4G27020,AT4G27040,AT5G15920,AT5G16690,AT5G22820,AT5G63380,AT5G63960,AT5G65210
+# stringdb - UniProt annotated keywords: KW-0925	- Oxylipin biosynthesis - 2 of 19	2.21	0.66;	FDR=0.0394
 
-## RPV12+1 specific - 12 genes
+## RPV12+1 specific - 33 genes
 dim(genotype_specific[which(genotype_specific$RPV12 > 0.05 & genotype_specific$`RPV12+1` < 0.05 & genotype_specific$`RPV12+1+3` > 0.05 & genotype_specific$p_batch > 0.05),])
 unique(sort(convTab[match(rownames(genotype_specific[which(genotype_specific$RPV12 > 0.05 & genotype_specific$`RPV12+1` < 0.05 & genotype_specific$`RPV12+1+3` > 0.05 & genotype_specific$p_batch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
-# no enrichment detected (string-db)
-# AT1G73830 (TF BEE3),AT1G76050,AT2G05710,AT2G40070,AT3G14770,AT3G48000,AT3G61690,AT5G03910,AT5G58760
+# Athal homologs: AT1G04420,AT1G14860,AT1G17980,AT1G32240,AT1G48580,AT1G73830,AT1G76050,AT2G17480,AT2G24610,AT2G25530,AT2G26170,AT2G35035,AT2G40070,AT3G03810,AT3G09320,AT3G14770,AT3G15380,AT3G26430,AT3G48000,AT3G61690,AT4G21250,AT4G36930,AT5G02410,AT5G03910,AT5G18410,AT5G36930,AT5G58760
+# no string-db term enriched
 
-## RPV12+1+3 specific - 23 genes
+## RPV12+1+3 specific - 66 genes
 dim(genotype_specific[which(genotype_specific$RPV12 > 0.05 & genotype_specific$`RPV12+1` > 0.05 & genotype_specific$`RPV12+1+3` < 0.05 & genotype_specific$p_batch > 0.05),])
 unique(sort(convTab[match(rownames(genotype_specific[which(genotype_specific$RPV12 > 0.05 & genotype_specific$`RPV12+1` > 0.05 & genotype_specific$`RPV12+1+3` < 0.05  & genotype_specific$p_batch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
-# no enrichment detected (string-db) 
-# AT1G04730,AT1G11910,AT1G74160,AT2G16950,AT2G34930,AT3G03080,AT3G11210,AT3G16350,AT3G54380,AT3G59140,AT3G60750,AT4G04790,AT4G11670,AT4G15570,AT4G17610,AT4G21470,AT4G29000,AT5G08430,AT5G14210,AT5G19210,AT5G23960,AT5G36890
+# Athal homologs: AT1G03270,AT1G06590,AT1G10200,AT1G11910,AT1G12570,AT1G13130,AT1G14870,AT1G19330,AT1G25530,AT1G30520,AT1G33270,AT1G33290,AT1G35530,AT1G49880,AT1G60730,AT1G66750,AT1G74160,AT2G03800,AT2G04160,AT2G18900,AT2G25100,AT2G34930,AT2G38695,AT2G45290,AT3G03080,AT3G11210,AT3G13730,AT3G14470,AT3G16350,AT3G18670,AT3G21690,AT3G25220,AT3G26700,AT3G33520,AT3G54380,AT3G59140,AT3G60750,AT3G63240,AT4G11670,AT4G13750,AT4G17610,AT4G21470,AT4G24050,AT4G29000,AT5G08430,AT5G11720,AT5G14210,AT5G16000,AT5G19210,AT5G23960,AT5G36890,AT5G36950,AT5G40820,AT5G47820,AT5G49160,AT5G50740,AT5G53080,AT5G54160,AT5G54590,AT5G62620,AT5G64030,AT5G66410
+# poor journal - skip - PubMed: PMID:35198585	 (2021) Multi-Omics Landscape of DNA Methylation Regulates Browning in Fuji Apple. - 4 of 14	2.1	0.72;	FDR=0.0069
 
-## all 3 genotypes - FDR<0.05 - 10 genes
+## all 3 genotypes - FDR<0.05 - 14 genes
 dim(genotype_specific[which(genotype_specific$RPV12 < 0.05 & genotype_specific$`RPV12+1` < 0.05 & genotype_specific$`RPV12+1+3` < 0.05 & genotype_specific$p_batch > 0.05),])
 unique(sort(convTab[match(rownames(genotype_specific[which(genotype_specific$RPV12 < 0.05 & genotype_specific$`RPV12+1` < 0.05 & genotype_specific$`RPV12+1+3` < 0.05 & genotype_specific$p_batch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
-# no enrichment detected
-# AT1G01760,AT2G07050,AT4G27290,AT4G30210,AT4G35560,AT4G37930,AT5G06120,AT5G23210,AT5G37590,AT5G42800
+# Atha homologs: AT1G34320,AT1G60690,AT2G07050,AT2G20410,AT4G27290,AT4G30210,AT4G35560,AT4G37930,AT5G05560,AT5G23210,AT5G35930,AT5G37590,AT5G42800
+# no string-db term enriched
 
-## Rpv12 and Rpv12+1+3 genotypes - FDR<0.05 - 7 genes
+## Rpv12 and Rpv12+1+3 genotypes - FDR<0.05 - 9 genes
 dim(genotype_specific[which(genotype_specific$RPV12 < 0.05 & genotype_specific$`RPV12+1` > 0.05 & genotype_specific$`RPV12+1+3` < 0.05 & genotype_specific$p_batch > 0.05),])
 unique(sort(convTab[match(rownames(genotype_specific[which(genotype_specific$RPV12 < 0.05 & genotype_specific$`RPV12+1` > 0.05 & genotype_specific$`RPV12+1+3` < 0.05 & genotype_specific$p_batch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
 # no enrichment detected
-# AT1G26320,AT1G35710,AT1G53440,AT3G14470,AT3G49000,AT3G63470
-# PPI enrichment p-value:	0.0415 (string-db)
+# AT1G08830, AT1G12740, AT1G26320, AT1G35710, AT1G53440, AT2G32530, AT3G49000, AT3G63470
+# PPI enrichment p-value:	0.0911 (string-db)
 
-## Rpv12+1 and Rpv12+1+3 genotypes - FDR<0.05 - 5 genes
+## Rpv12+1 and Rpv12+1+3 genotypes - FDR<0.05 - 13 genes
 dim(genotype_specific[which(genotype_specific$RPV12 > 0.05 & genotype_specific$`RPV12+1` < 0.05 & genotype_specific$`RPV12+1+3` < 0.05 & genotype_specific$p_batch > 0.05),])
 unique(sort(convTab[match(rownames(genotype_specific[which(genotype_specific$RPV12 > 0.05 & genotype_specific$`RPV12+1` < 0.05 & genotype_specific$`RPV12+1+3` < 0.05 & genotype_specific$p_batch > 0.05),]), convTab$PN40024_genotype_ENSMBL_ID), 12]))
-# no enrichment detected
-# AT1G49350,AT1G69550,AT2G43420,AT4G02460,AT5G05670
+# Athal homologs: AT1G17350,AT1G26160,AT1G28210,AT1G49350,AT2G43420,AT3G58140,AT3G63250,AT4G02460,AT4G31940,AT5G05670,AT5G38690,AT5G48830,AT5G51660
+# no enrichment found
 
 ########### EXPLORATORY ANALYSIS ###################
 #### all susceptible samples must have 0 SJ, all resistant samples must have more than 0 SJs
@@ -324,21 +320,17 @@ cond_other_all_positive <- rowSums(SJmat[, other_idx] > 0) == length(other_idx)
 valid_rows <- which(cond_susc_zero & cond_other_all_positive)
 SJmat_clean <- SJmat[valid_rows, ]
 dim(SJmat_clean) # 45
-# test adj p-values
+## test adj p-values for genotype importance
 suscp_resist_SJ_diff <- AdjPvalues[rownames(SJmat_clean),]
-suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),] # 7 genes
-# adjPgen adjPtim adjPgen_tim
-# Vitvi00g04449 1.822692e-04       1           1
-# Vitvi02g01621 2.124787e-04       1           1
-# Vitvi09g01592 4.557770e-11       1           1
-# Vitvi12g01939 1.771809e-07       1           1
-# Vitvi14g02707 2.117467e-02       1           1
-# Vitvi15g01247 1.049532e-26       1           1
-# Vitvi18g04655 1.978497e-03       1           1
+suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),] # 45 genes
 convTab[match(rownames(suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),]), convTab$PN40024_genotype_ENSMBL_ID),]
-convTab[match(rownames(suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),]), convTab$PN40024_genotype_ENSMBL_ID),12]
-# AT2G22910, AT5G22300, AT5G06220, AT1G35710, AT4G27220, AT5G07360, AT5G17680
-# Evidence of resistance-specific splicing switches
+unique(sort(convTab[match(rownames(suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),]), convTab$PN40024_genotype_ENSMBL_ID),12]))
+# Athal homologs: AT1G01620,AT1G18010,AT1G35710,AT1G71500,AT1G78080,AT2G14110,AT2G22540,AT2G22910,AT3G04220,AT3G12800,AT3G46530,AT3G59470,AT4G10930,AT4G12010,AT4G21580,AT4G24540,AT4G27220,AT5G06220,AT5G07360,AT5G17680,AT5G22300,AT5G45230,AT5G45250
+# PPI enrichment p-value:	0.0738
+# Molecular Function: GO:0043531 - ADP binding - 7 of 175	1.68	2.2;	FDR=3.56e-07
+# RPS4-2, K18C1.11, RPP13, DSC1, MVA3.30, T6K12.1, F14D7.1, M4I22.30 
+# UniProt - KW-0433 - Leucine-rich repeat - 7 of 593	1.15	1.06;	FDR=0.00019
+# InterPro - IPR042197/IPR002182 - Apoptotic protease-activating factors, helical domain / NB-ARC - 7 of 147	1.75	2.37;	FDR=2.18e-07
 
 #### all susceptible samples must have more than 0 SJ, all resistant samples must have  0 SJs
 # Condition A: all susceptible columns are zero
@@ -352,8 +344,9 @@ unique(sort(convTab[match(rownames(SJmat_clean), convTab$PN40024_genotype_ENSMBL
 dim(SJmat_clean) # 12 genes
 # test adj p-values
 suscp_resist_SJ_diff <- AdjPvalues[rownames(SJmat_clean),]
-suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),]
-# Vitvi08g01235 8.658164e-13       1           1  - LBO1 - AT3G21420
-# Vitvi12g04497 1.810544e-03       1           1  - SEC6 - AT1G71820
+suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),] # 12 genes
 convTab[match(rownames(suscp_resist_SJ_diff[which(suscp_resist_SJ_diff$adjPgen < 0.05),]), convTab$PN40024_genotype_ENSMBL_ID),]
+unique(sort(convTab[match(rownames(SJmat_clean), convTab$PN40024_genotype_ENSMBL_ID),2]))
+# F6H4C7, F6H4C8, F6H4C9, F6H4D0, F6H4D1, F6H928, F6HL84
+# InterPro - IPR020783 - Ribosomal protein L11, C-terminal - 2 of 6	3.15	1.35;	FDR=0.0079
 
